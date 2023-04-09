0463f1d9e07ae6794d88f44c755df85ad8e5462f
thaothang <tranthithao1696@gmail.com>
2023-04-09 23:47:56 +0700

    commnit topic22

--- src/Topic_03.java
+++ src/Topic_03.java
@@ -54,6 +54,30 @@ public class Topic_03 {
 		//Verifys
 		Assert.assertEquals(driver.findElement(By.id("txtCPassword-error")).getText(), "Mật khẩu phải có ít nhất 6 ký tự");
 	}
+	
+	@Test
+	public void TC_06_InvalidPhone() {
+		driver.get("https://alada.vn/tai-khoan/dang-ky.html");
+		//Action1
+		driver.findElement(By.id("txtFirstname")).sendKeys("Trần Thị Thảo");
+		driver.findElement(By.id("txtEmail")).sendKeys("tranthithao@gmail.com");
+		driver.findElement(By.id("txtCEmail")).sendKeys("tranthithao@gmail.com");
+		driver.findElement(By.id("txtPhone")).sendKeys("0961845598");	
+		driver.findElement(By.id("txtPassword")).sendKeys("111111");
+		driver.findElement(By.id("txtCPassword")).sendKeys("111111");
+		driver.findElement(By.id("txtPhone")).sendKeys("096184559");
+		driver.findElement(By.xpath("//form[@id='frmLogin']//button[text()='ĐĂNG KÝ']")).click();
+	
+		//Verify1
+		Assert.assertEquals(driver.findElement(By.id("txtPhone-error")).getText(), "Số điện thoại phải từ 10-11 số.");
+		
+		//Action2
+		driver.findElement(By.id("txtPhone")).clear();
+		driver.findElement(By.id("txtPhone")).sendKeys("06184559");
+		
+		//Verify2
+		Assert.assertEquals(driver.findElement(By.id("txtPhone-error")).getText(), "Số điện thoại bắt đầu bằng: 09 - 03 - 012 - 016 - 018 - 019 - 088 - 03 - 05 - 07 - 08");
+	}
 //
 //	@Test
 //	public void TC_03_Form() {
